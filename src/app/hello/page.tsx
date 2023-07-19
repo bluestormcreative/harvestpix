7402bda958126670888875c07b1dbafce56988ca
Max MacMillan <max@treat.xyz>
2023-07-19 12:28:43 -0400

    Make sure server component still loads

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
-export default function Page() {
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return <div>Hi there</div>;
 }
