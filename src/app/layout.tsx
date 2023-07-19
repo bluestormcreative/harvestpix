b548c37e6008966eba66909f73364eebcc22f214
Max MacMillan <max@treat.xyz>
2023-07-19 12:21:32 -0400

    Wrap 3rd-party components in use client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Container } from "@/components/bootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>shared div</div>
-        {children}
+        <Container>{children}</Container>
       </body>
     </html>
   );
