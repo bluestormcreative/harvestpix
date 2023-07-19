008d27eb43f3a67b1f3f5f713280274a996533c2
Max MacMillan <max@treat.xyz>
2023-07-19 12:28:28 -0400

    Wrap tree in SSRProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Container } from "@/components/bootstrap";
+import { Container, SSRProvider } from "@/components/bootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Container>{children}</Container>
+        <SSRProvider>
+          <main>
+            <Container className="py-4">{children}</Container>
+          </main>
+        </SSRProvider>
       </body>
     </html>
   );
