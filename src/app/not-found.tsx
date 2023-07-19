f8d7ba15558a50955b6f593f9da9e95c867c6b68
Max MacMillan <max@treat.xyz>
2023-07-19 13:17:21 -0400

    Escape quote

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div>
       <h1>Whoops</h1>
-      <p>That page doesn't exist.</p>
+      <p>That page doesn&#39;t exist.</p>
     </div>
   );
 }
